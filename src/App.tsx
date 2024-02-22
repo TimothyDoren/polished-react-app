faf6dc7e9dcda28748465a3ec6d2853e91fc67dc
Tim D <124634979+TimothyDoren@users.noreply.github.com>
2024-02-21 22:15:08 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Homepage from "./Homepage"
+
+function App(){
+  return <div><Homepage /></div>;
+}
+
+export default App;
---
+++ src/Homepage.tsx
@@ -0,0 +1,5 @@
+function Homepage(){
+    return <h1>Polished Automotive</h1>;
+}
+
+export default Homepage;
