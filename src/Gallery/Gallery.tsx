401e68bfe98b69a2dbbb6a7a2083820abc03abb2
Tim D <124634979+TimothyDoren@users.noreply.github.com>
2024-02-29 18:08:39 -0500

    added new pics

--- src/Gallery/Gallery.tsx
+++ src/Gallery/Gallery.tsx
@@ -3,11 +3,20 @@ import ImageGallery from 'react-image-gallery'
 import "react-image-gallery/styles/css/image-gallery.css";
 import './Gallery.css'
 
-import carImage1 from '../assets/images/cars/blueCar1-med.jpg';
-import carImage2 from '../assets/images/cars/blueCar2-med.jpg';
-import carImage3 from '../assets/images/cars/blackCar1-med.jpg';
-import carImage4 from '../assets/images/cars/redCar-med.jpg';
-import carImage5 from '../assets/images/cars/yellowCar-med.jpg';
+import carImage1 from '../assets/images/cars/blackCar1.jpg';
+import carImage2 from '../assets/images/cars/blackCar2.jpg';
+import carImage3 from '../assets/images/cars/blackCar3.jpg';
+import carImage4 from '../assets/images/cars/blackCar4.jpg';
+import carImage5 from '../assets/images/cars/blackCarWheel1.jpg';
+import carImage6 from '../assets/images/cars/Carinterior.jpg';
+import carImage7 from '../assets/images/cars/cars1.jpg';
+import carImage8 from '../assets/images/cars/cars2.jpg';
+import carImage9 from '../assets/images/cars/joshCleaning.jpg';
+import carImage10 from '../assets/images/cars/Mobile Detailing Cincinnati.jpg';
+import carImage11 from '../assets/images/cars/redCar1.jpg';
+import carImage12 from '../assets/images/cars/redCar2.jpg';
+import carImage13 from '../assets/images/cars/whiteCar1.jpg';
+import carImage14 from '../assets/images/cars/twoBlackCars.jpg';
 
 const images = [
     { original: carImage1 },
@@ -15,6 +24,15 @@ const images = [
     { original: carImage3 },
     { original: carImage4 },
     { original: carImage5 },
+    { original: carImage6 },
+    { original: carImage7 },
+    { original: carImage8 },
+    { original: carImage9 },
+    { original: carImage10 },
+    { original: carImage11 },
+    { original: carImage12 },
+    { original: carImage13 },
+    { original: carImage14 },
 ];
 
 function Gallery(){
